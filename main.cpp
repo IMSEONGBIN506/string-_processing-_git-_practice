8a9533259273770f37c6c31bbd488664f9571afa
IMSEONGBIN506 <bins506@gmail.com>
2024-05-23 19:46:18 +0900

    sorry

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 #include "uppercase.h"
+
 using namespace std;
+
 int main() {
     string text = "Hello, World!";
     string upperText = toUpperCase(text);
     cout << "Upper case: " << upperText << endl;
     return 0;
 }
+
+// 권채은, 이유현 바보 멍청이 똥개
