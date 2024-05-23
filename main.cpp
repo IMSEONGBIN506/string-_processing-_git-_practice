7267ffc63b33b46f4e70c4ad0d3d03a446a3ea29
LYH513 <peperony367@gmail.com>
2024-05-23 19:38:24 +0900

    끝

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include "uppercase.h"
+#include "reverse.h"
 using namespace std;
 int main() {
     string text = "Hello, World!";
     string upperText = toUpperCase(text);
     cout << "Upper case: " << upperText << endl;
-    return 0;
-}
+    string reversedText = reverseString(text);
+    cout << "Reversed: " << reversedText << endl;
+return 0; }
